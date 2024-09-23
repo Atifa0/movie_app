25d052db8bf14f70cf28a2cba521b6108bbdf8d6
farah302 <ffarahmahhmoud@gmail.com>
2024-09-23 18:36:09 +0300

    Home_Screen ui

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,14 +1,131 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Home_Screen extends StatelessWidget {
   const Home_Screen({super.key});
   static const String RouteName = 'Home_Screen';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Home_Screen'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          Icon(Icons.search),
+          SizedBox(width: 15),
+          Icon(Icons.notifications),
+          SizedBox(width: 15),
+        ],
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FeaturedSection(),
+          SizedBox(height: 20),
+          CategorySection(title: 'New Releases'),
+          SizedBox(height: 20),
+          CategorySection(title: 'Recommended'),
+        ],
+      ),
+
+    );
+  }
+}
+
+class FeaturedSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[800],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Center(
+              child: Icon(
+                Icons.play_circle_outline,
+                color: Colors.white,
+                size: 50,
+              ),
+            ),
+          ),
+          Text(
+            'Dora and the Lost City of Gold',
+            style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 5),
+          Text(
+            '2019  PG-13  2h 7m',
+            style: TextStyle(color: Colors.white70, fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+}
+class CategorySection extends StatelessWidget {
+  final String title;
+
+  CategorySection({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          height: 150,
+          child: Scrollbar(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 100,
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[800],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.movie, color: Colors.white, size: 50),
+                      SizedBox(height: 10),
+                      Text(
+                        'Title $index',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Rating',
+                        style: TextStyle(color: Colors.white70, fontSize: 10),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       routes: {
         SplashScreen.RouteName: (context) => SplashScreen(),
         Home.RouteName: (context) => Home(),
-        Home_Screen.RouteName: (context) => Home_Screen(),
+        Home_Screen.RouteName : (context) => Home_Screen(),
         Search_Screen.RouteName: (context) => Search_Screen(),
         Browse.RouteName: (context) => Browse(),
         WatchList.RouteName: (context) => WatchList(),
